98202a32c457658c5864e231298fb8afc907d64e
athizz2005 <atharva15.bgmi@gmail.com>
2023-12-24 14:31:04 +0000

    violet : Bringup @ProjectMatrixx

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -13,10 +13,26 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # MiuiCamera
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
-# Inherit some common crDroid stuff
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit some common device props
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_OEM_CAMERA := true
+TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_DISABLE_EPPE := true
+WITH_GAPPS := true
+MATRIXX_CHIPSET := VIOLET
+MATRIXX_BATTERY := 4000mah
+TARGET_HAS_UDFPS := false
+MATRIXX_BUILD_TYPE := Unofficial
+MATRIXX_MAINTAINER := AtharvaSwamy
+MATRIXX_DISPLAY := 1080x2349
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_violet vendor/xiaomi/violet --depth=1
+git clone https://github.com/Ancient-Project/android_kernel_xiaomi_violet -b 14 kernel/xiaomi/violet --depth=1
+cd kernel/xiaomi/violet
+git apply dsp*
+cd ../../..
+git clone --depth=1 https://gitlab.com/kibria5/vendor_xiaomi-firmware_violet.git -b thirteen vendor/xiaomi-firmware/violet
+git clone --depth=1 https://gitlab.com/kibria5/prebuilts_clang_host_linux-x86_clang-r510928 prebuilts/clang/host/linux-x86/clang-r510928
+git clone https://gitlab.com/NRanjan-17/android_vendor_MiuiCamera vendor/MiuiCamera --depth=1
