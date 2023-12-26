5fbc435fa054ccd3d47a0d356dad2efc1dd607a3
Atharva Swamy <111036482+athizz2005@users.noreply.github.com>
2023-12-26 08:21:22 +0530

    violet : inherit kernel from crdroid

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,5 @@
 git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_violet vendor/xiaomi/violet --depth=1
-git clone https://github.com/Ancient-Project/android_kernel_xiaomi_violet -b 14 kernel/xiaomi/violet --depth=1
-cd kernel/xiaomi/violet
-git apply dsp*
-cd ../../..
+git clone https://github.com/crdroidandroid/android_kernel_xiaomi_violet -b 14.0 kernel/xiaomi/violet --depth=1
 git clone --depth=1 https://gitlab.com/kibria5/vendor_xiaomi-firmware_violet.git -b thirteen vendor/xiaomi-firmware/violet
 git clone --depth=1 https://gitlab.com/kibria5/prebuilts_clang_host_linux-x86_clang-r510928 prebuilts/clang/host/linux-x86/clang-r510928
 git clone https://gitlab.com/NRanjan-17/android_vendor_MiuiCamera vendor/MiuiCamera --depth=1
