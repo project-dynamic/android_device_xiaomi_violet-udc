a92dd4d0f1891da0ff949a0b72d9a05496500657
athizz2005 <atharva15.bgmi@gmail.com>
2024-01-06 04:36:37 +0000

    violet : Bringup @SuperiorExtended

--- superior_violet.mk
+++ superior_violet.mk
@@ -8,13 +8,29 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common SuperiorOS stuff.
+# Inherit some common SuperiorExtendedOS stuff.
 $(call inherit-product, vendor/superior/config/common.mk)
-SUPERIOR_OFFICIAL := true
+
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SHIPS_PREBUILT_GCAM := false
 TARGET_INCLUDE_MATLOG := true
-TARGET_INCLUDE_PIXEL_CHARGER := true
 USE_MOTO_CALCULATOR := true
+TARGET_SUPPORTS_BLUR := true
+SUPERIOR_UDFPS_ANIMATIONS := false
+USE_MOTO_CLOCK := false
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+BUILD_WITH_GAPPS := false
+USE_QUICKPIC := true
+USE_DUCKDUCKGO := false
+USE_ViaBrowser := true
+
+# MiuiCamera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
+# Maintainer
+PRODUCT_SYSTEM_PROPERTIES += \
+       ro.spos.maintainer=AtharvaSwamy
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := superior_violet
