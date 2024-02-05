b48a64b185a39ba62c3d0eb341c58998fdbfe47f
athizz2005 <atharva15.bgmi@gmail.com>
2024-02-05 13:25:52 +0000

    violet: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/project-dynamic/android_vendor_xiaomi_violet-udc vendor/xiaomi/violet --depth=1
+git clone https://github.com/Ancient-Project/android_kernel_xiaomi_violet -b 14 kernel/xiaomi/violet  --depth=1
+cd kernel/xiaomi/violet
+git apply dsp*
+cd ../../..
+git clone --depth=1 https://gitlab.com/kibria5/vendor_xiaomi-firmware_violet.git -b fourteen vendor/xiaomi-firmware/violet
+git clone https://gitlab.com/NRanjan-17/android_vendor_MiuiCamera vendor/MiuiCamera --depth=1
